cda354b6c358731e28ffdab7db7bd8a445d88061
Tami Sweitzer <tamara0715@gmail.com>
2018-09-25 16:18:57 -0500

    Added the CSharpMethods file, which lists the most common methods and code samples that I will be needing

---
+++ Reference_CSharpMethods/Methods.cs
@@ -0,0 +1,33 @@
+
+// An alert box
+MessageBox.Show("Hello world!");
+
+// To put text into a label control
+myLabel.Text = "Hi there!";
+
+// To clear the  text in a label
+myLabel.Text = "";
+
+// To initialize an emoty string
+string myString = string.Empty;
+string myString = "";
+
+// To manage visibility of a control
+myLabel.Visible = true;
+myLabel.Visible = false;
+
+// To close an application
+this.Close();
+
+/*
+//Ch 3.2 Variables p. 120
+Primitive data types
+
+string
+
+Local variable - a variable declared inside of a method
+is local to that method
+
+
+
+*/
