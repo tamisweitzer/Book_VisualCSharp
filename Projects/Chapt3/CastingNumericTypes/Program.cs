995e8eae93df1cca4a4964147a7533233dee88c0
tamara0715@gmail.com <tamara0715@gmail.com>
2018-10-04 22:33:57 -0500

    add new folder containing projects from working through chapter3 of the book

---
+++ Projects/Chapt3/CastingNumericTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastingNumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // what is the difference between cast int(VARIABLE) and int.Parse(VARIABLE)
+
+            // P 135
+            int wholeNumber = 3;
+            double realNumber = 3.0;
+            decimal decimalNumber = 3.1m;
+
+            // cast the double to an int
+            Console.WriteLine((int)realNumber);         // 3
+            Console.WriteLine((int)decimalNumber);      // 3
+            Console.WriteLine((double)wholeNumber);     // 3
+            Console.WriteLine((double)decimalNumber);   // 3.1
+            Console.WriteLine((decimal)realNumber);     // 3
+            Console.WriteLine((decimal)wholeNumber);    // 3
+
+ 
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Projects/Chapt3/Chapt3_3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapt3_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal myDecimal = 3.00m;
+
+
+            Console.WriteLine(myDecimal.ToString());
+            Console.ReadLine();
+        }
+    }
+}
