6440f09bcfc0a6dbf9ede84088a07e3d67097b84
Tami Sweitzer <tamara0715@gmail.com>
2018-10-08 23:44:01 -0500

    3.9 Named Constants

---
+++ Projects/Chapt3/3.9_NamedConstants/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._9_NamedConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Chapter 3.8 Using Named Constants
+            // A named constant is a 'variable' whose value cannot be changed by other code
+            // Declaration:     const double INTEREST_RATE  = 0.129;
+            // The 'const' keyword tells the compliler to make the variable read only
+            // Because you cannot change the value of the CONST later in the code, you must initialize it when you create it
+
+        }
+    }
+}
