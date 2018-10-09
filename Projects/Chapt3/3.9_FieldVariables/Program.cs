0a52a20d58d88dd529fd583ae84cfd0083148f7f
Tami Sweitzer <tamara0715@gmail.com>
2018-10-09 00:22:48 -0500

    Stopping for the night. Left off working on const fields, p.170

--- Projects/Chapt3/3.9_FieldVariables/Program.cs
+++ Projects/Chapt3/3.9_FieldVariables/Program.cs
@@ -12,11 +12,18 @@ namespace _3._9_FieldVariables
         // so that they are accessible to all of the following methods and code 
 
         // Fields are declared like local variables, plus they have a modifier
-        private int number = 0; 
+        private int number = 0;
         // The private modifier means that any code outside of the class cannot see it
         // but any code inside the class, including methods, can see it
 
+        // Lifetime of a field
+        // the lifetime of a field variable is the entire time that the class exists
+        // by comparison, a regular variable's lifetime is only while the method is running
 
+        // Constant Fields
+        // fields for whose values cannot be changed
+        // create it like a normal const, and remember to declare it private
+        private const string MY_CONST_FIELD = "I cannot be changed!";
 
 
 
